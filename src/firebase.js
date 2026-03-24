82e1dc70d88da90455e1d844381b41d1bbbfd77d
Tomasn05 <tomasnotti05@gmail.com>
2026-03-24 20:45:07 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDSbhqlMWy4yaInrfKMTljfMGT8_2iXJWs",
@@ -11,4 +12,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
+export const db = getFirestore(app);
